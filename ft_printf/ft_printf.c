f7ed2722e881acbbbb02fe66def0b0905b8e1327
ggila <ggilaber@student.42.fr>
2015-12-03 21:03:23 +0100

    printf

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/03 18:13:24 by ggilaber          #+#    #+#             */
+/*   Updated: 2015/12/03 20:31:14 by ggilaber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "unistd.h"
+#include <stdarg.h>
+
+#define OK 0
+#define KO 0
+
+void	write_arg(int fd, const char **str, va_list args);
+
+void	my_write(int fd, const char *str, size_t len);
+{
+	if (write(fd, str, len) == -1)
+		exit(KO)
+}
+
+int	ft_print_fd(int fd, const char *restrict format, ...)
+{
+	const char	*begin;
+	const char	*end;
+	va_list		args;
+
+	va_start(args, format);
+	begin = format;
+	while (*begin)
+	{
+		end = begin;
+		while (*end && *end != '%' && *end != '\\')
+			end++;
+		my_write(fd, begin, end - begin);
+		if (*end == '\0')
+			break;
+		*end == '\\' ? my_write(fd, ++end, 1) : write_arg(fd, &end, args);
+		begin = ++end;
+	}
+	va_end(args);
+	if (g_flag & MASK_COLOR)
+		write(fd, WHITE, COLOR_LEN)
+	return (OK);
+}
+
+int	ft_print(const char *restrict format, ...)
+{
+	const char	*begin;
+	const char	*end;
+	va_list		args;
+
+	va_start(args, format);
+	begin = format;
+	while (*begin)
+	{
+		end = begin;
+		while (*end && *end != '%' && *end != '\\')
+			end++;
+		my_write(0, begin, end - begin);
+		if (*end == '\0')
+			break;
+		*(end++) == '\\' ? my_write(0, end, 1) : write_arg(0, &end, args);
+		begin = ++end;
+	}
+	va_end(args);
+	if (g_flag & MASK_COLOR)
+		write(fd, WHITE, COLOR_LEN)
+	return (OK);
+}
