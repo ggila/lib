ba6c251059b22af0c09dfaa9aa6647dd2d2d8f35
ggila <ggilaber@student.42.fr>
2016-01-18 16:25:16 +0100

    init symbol table

---
+++ README.md
@@ -0,0 +1,4 @@
+
+void	st_put(t_st simtab, char *key, void *val);
+void	*st_get(t_st simtab, char *key);
+void	st_delete();
--- ft_printf/main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-
-int main(void)
-{
-	ft_printf("abc\n\\\\\\\n%s\n%c%c%c%c\n", "abc",'a','a','a','a');
-	ft_printf("\n");
-	ft_printf("%d\n%s: %d\nf: %f\n%.5f\n",2,"line",4,1.2f, 1.23456f);
-	return (0);
-}
