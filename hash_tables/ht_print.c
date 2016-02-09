2997e28b33bc9440ad00ef2ca260dcf914b090cc
ggila <ggilaber@student.42.fr>
2016-02-09 16:00:43 +0100

    printf -> ft_printf

--- hash_tables/ht_print.c
+++ hash_tables/ht_print.c
@@ -6,24 +6,24 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/06 17:19:16 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/08 19:13:21 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/02/09 16:00:16 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hash_tables.h"
-#include <stdio.h>
+#include "ft_printf.h"
 
 #define NODE_HEADER "---------------------\nnode n*%lu:\n"
 #define NODE_SEP "---/\n  /\n /-->"
 
 void			str_print(const char *str)
 {
-	printf("%s", str);
+	ft_printf("%s", str);
 }
 
 void			int_print(const int *i)
 {
-	printf("%d", *i);
+	ft_printf("%d", *i);
 }
 
 static void	node_print(t_hash_node *node, void (*key_print)(),
@@ -31,18 +31,18 @@ static void	node_print(t_hash_node *node, void (*key_print)(),
 {
 	while (node)
 	{
-		printf("addr: %p\n", node);
-		printf("key: ");
+		ft_printf("addr: %p\n", node);
+		ft_printf("key: ");
 		key_print(node->kv.key);
-		printf("\nkey size: %lu\n", node->kv.key_size);
-		printf("value: ");
+		ft_printf("\nkey size: %d\n", (int)node->kv.key_size);
+		ft_printf("value: ");
 		value_print(node->kv.value);
-		printf("\nvalue size: %lu\n", node->kv.value_size);
-		printf("next: ");
+		ft_printf("\nvalue size: %d\n", (int)node->kv.value_size);
+		ft_printf("next: ");
 		if (node->next == NULL)
-			printf("NULL\n");
+			ft_printf("NULL\n");
 		else
-			printf("%p\n%s", node->next, NODE_SEP);
+			ft_printf("%p\n%s", node->next, NODE_SEP);
 		node = node->next;
 	}
 }
@@ -55,15 +55,15 @@ void		ht_print(t_hash_tbl *ht, void (*key_print)(),
 	i = -1;
 	if (ht)
 	{
-		printf("size: %lu\n", ht->size);
+		ft_printf("size: %d\n", (int)ht->size);
 		while (++i < ht->size)
 		{
-			printf(NODE_HEADER, i);
+			ft_printf(NODE_HEADER, i);
 			if (ht->nodes[i] == NULL)
-				printf("NULL\n");
+				ft_printf("NULL\n");
 			else
 				node_print(ht->nodes[i], key_print, value_print);
 		}
-		printf("---------------------\n");
+		ft_printf("---------------------\n");
 	}
 }
