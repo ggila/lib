98af23d0d0afe99a506c9f5761658b02cb491129
ggila <ggilaber@student.42.fr>
2016-02-12 09:24:08 +0100

    opt

--- options/check_options.c
+++ options/check_options.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 21:40:57 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/11 22:37:56 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/02/12 09:21:13 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,67 @@
 */
 
 #include <stdint.h>
+#include <stdlib.h>
+
+#include "libbft.h"
 #include "options.h"
+#include "ft_printf.h"
 
-static const t_char_opt	char_opt[] = {{'h', MASK_HELP},
-										NULL_CHAR_OPT};
+extern uint64_t	g_flag;
 
-static const t_str_opt	string_opt[] = {{"help", MASK_HELP},
-							NULL_STR_OPT};
-
-extern uint64_t	flag;
-
-static void		init_hash_tbl(const t_hash_tlb *ht_char,
-								const t_hash_tlb *ht_str);
+static void	char_opt(char *arg)
 {
-	ht_init(ht_char, HT_CHAR_SIZE);
-	ht_init(ht_str, HT_STR_SIZE);
+	int i = 0;
+	int j = 0;
+
+	while (arg[i])
+	{
+		while (str_op[j] && (str_op[j] != arg[i]))
+			j++;
+		if (str_op[j])
+			g_flag &= (MASK<<j);
+		else
+			return (KO);
+		i++;
+	}
+	return (OK);
+}
+
+static void	str_opt(char *arg)
+{
+	int i = 0;
+
+	while (str_op[i] && ft_strcmp(str_op[i], arg))
+		i++;
+	if (str_op[i])
+		g_flag &= (MASK<<(63 - i));
+	else
+		return (KO);
+	return (OK);
+}
+
+static char	is_option(char *arg)
+{
+	if ((arg[0] != '-') || (ft_strcmp(arg, "--") == 0))
+		return (KO);
+	return (OK);
 }
 
 int		check_opt(int ac, const char **av)
 {
-	t_hash_tbl	ht_char;
-	t_hash_tbl	ht_str;
-
-	init_hash_tbl(&ht_char, &ht_str);
+	i = 1;
+	while (i < ac)
+	{
+		if (is_option(av[i]) == KO)
+			break;
+		if ((av[i][1] == '-' ? str_opt(av[i] + 2) :
+								char_opt(av[i] + 1)) == KO)
+		{
+			ft_printf("%s: illegal option: %s\n", av[0], av[i]);
+			usage(av[0]);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (i);
 }
-char			ht_init(t_hash_tbl *const ht, int size,
-char			ht_init(t_hash_tbl *const ht, int size,
-					int (*key_cmp)(), unsigned long (*key_hash)());
-
--- options/options.h
+++ options/options.h
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 21:50:28 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/11 22:37:05 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/02/12 09:19:20 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,27 @@
 **     uint64_t   g_flag;
 */
 
+typdef struct	s_char_opt
+{
+	char	opt;
+	char	*comment;
+}				t_char_opt;
 
-# include "hash_tables.h"
+typdef struct	s_str_opt
+{
+	char	*opt;
+	char	*comment;
+}				t_str_opt;
+
+static const t_char_opt char_opt[] = {{'h', "display this help"},
+										{0, NULL}};
+
+static const t_str_opt str_opt[] = {{"help", "display this help"},
+										{NULL, NULL}};
 
 # define MASK 0x1
 # define MASK_HELP MASK<<0
-
-typedef struct	s_char_opt
-{
-	char			c;
-	unsigned int	mask;
-}				t_char_opt;
-
-# define NULL_CHAR_OPT (t_char_opt){0, 0}
-# define HT_CHAR_SIZE 5
-
-typedef struct	s_str_opt
-{
-	char			*str;
-	unsigned int	mask;
-}				t_str_opt;
-
-# define NULL_STR_OPT (t_str_opt){NULL, 0}
-# define HT_STR_SIZE 5
+# define MASK_HELP MASK<<63
 
 /*
 ** check options return the number of char* of av read as options or -1
@@ -52,5 +50,7 @@ typedef struct	s_str_opt
 */
 
 int		check_opt(int ac, const char **av);
+void	usage(const char *prog_name);
+void	print_help(const char *prog_name);
 
 #endif
