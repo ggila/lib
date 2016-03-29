ec80aeac10fcee56cb251661b1bed1bab961a2a1
ggila <ggilaber@student.42.fr>
2016-03-29 21:27:55 +0200

    memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/30 17:07:14 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/03/23 11:56:29 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/03/29 21:27:46 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_memset(void *dst, int c, size_t len)
 	p2_dst = (char*)dst;
 	i = 0;
 	while (i < 4)
-		long_c = ((int64_t)c)<<(8*i);
+		long_c = ((int64_t)c)<<(8*i++);
 	while (8 * i < len)
 	{
 		p1_dst[i] = long_c;
