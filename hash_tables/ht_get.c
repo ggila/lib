3634516b5887c39a52c6bf7768362e718ad0ad8d
ggila <ggilaber@student.42.fr>
2016-05-27 17:35:23 +0200

    ht_get correction

--- hash_tables/ht_get.c
+++ hash_tables/ht_get.c
@@ -6,7 +6,7 @@
 /*   By: ggilaber <ggilaber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 21:07:26 by ggilaber          #+#    #+#             */
-/*   Updated: 2016/02/07 14:56:48 by ggilaber         ###   ########.fr       */
+/*   Updated: 2016/05/27 17:33:28 by ggilaber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	*ht_get(t_hash_tbl *ht, void *key)
 	t_hash_node		*node;
 
 	i = ht->key_hash(key) % ht->size;
-	node = ht->nodes[i];
+	if ((node = ht->nodes[i]) == NULL)
+		return (NULL);
 	while (ht->key_cmp(node->kv.key, key) && node->next)
 		node = node->next;
 	if (ht->key_cmp(node->kv.key, key) == 0)
